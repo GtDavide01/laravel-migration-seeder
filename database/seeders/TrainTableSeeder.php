42e0326236cb66c4de0ab5add9e342e5c67350d1
GtDavide01 <davidefiorini01@gmail.com>
2022-12-20 14:18:43 +0100

    aggiunte colonne tramite seeders

--- database/seeders/TrainTableSeeder.php
+++ database/seeders/TrainTableSeeder.php
@@ -18,13 +18,16 @@ class TrainTableSeeder extends Seeder
     {
         for ($i = 0; $i < 10; $i++) {
             $newTrain = new Train();
-            $newTrain->agency = $faker->words(1);
-            $newTrain->departure_station = $faker->words(1);
-            $newTrain->arrival_station = $faker->words(1);
+            $newTrain->agency = $faker->word();
+            $newTrain->departure_station = $faker->word();
+            $newTrain->arrival_station = $faker->word();
             $newTrain->departure_time = $faker->date('2022_12_d');
             $newTrain->arrival_time = $faker->date('2022_12_d');
             $newTrain->train_code = $faker->numberBetween(1, 1000);
-            $newTrain->numner_of_carriages = $faker->numberBetween(7, 30);
+            $newTrain->number_of_carriages = $faker->numberBetween(7, 30);
+            $newTrain->in_time = $faker->numberBetween(0, 1);
+            $newTrain->deleted = $faker->numberBetween(0, 1);
+            $newTrain->save();
         }
     }
 }
